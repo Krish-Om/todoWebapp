0e164cfa745237d9d16789c45cfc7c3234d07c7d
Krish-Om <Krishombasukala@gmail.com>
2024-06-02 22:28:43 +0545

    added a changed fetch method

--- todo/src/frontend/js/database.js
+++ todo/src/frontend/js/database.js
@@ -17,15 +17,35 @@ let obj;
 // },2000);
 const endpoint = 'http://localhost:8080/allTask';
 
+// let promise = fetch(endpoint);
+// console.log(promise);
 
-fetch(endpoint,{
-    method:"GET",
-})
-.then(responseBody =>
-    { responseBody.json;})
-.then((data) => {
-    console.log('Data from db',data)})
 
-.catch(error => {
-console.error('fetch error: ',error);
-})
+const getTaskFromDatabase = async ()=>{
+    // console.log("getting data");
+    let response = await fetch(endpoint);
+    let data = await response.json();
+    // obj = [...data];
+    console.log(data);
+    console.log(data[0].taskDetail);
+}
+getTaskFromDatabase();
+
+
+
+
+
+
+
+// console.log(obj);
+// fetch(endpoint,{
+//     method:"GET",
+// })
+// .then(responseBody =>
+//     { responseBody.json;})
+// .then((data) => {
+//     console.log('Data from db',data)})
+
+// .catch(error => {
+// console.error('fetch error: ',error);
+// })
